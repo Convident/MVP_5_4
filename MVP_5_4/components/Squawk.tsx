08606beeeae235598478850a92ec8070cd9a09d6
Nicholas <nkpqiu@gmail.com>
2024-05-04 17:31:48 -0500

    Update Squawk.tsx
    added firebase support for randomized questions

--- MVP_5_4/components/Squawk.tsx
+++ MVP_5_4/components/Squawk.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState, useEffect } from 'react';
 import MultipleChoiceQuestion from './MCQ'; // Import the question types from other files
 import TextQuestion from './SAQ';
 import { View, Text } from 'react-native';
@@ -12,45 +12,57 @@ interface QuestionContainerProps {
     questionType: string;
 }
 
-async function getCSVFileFromStorage (questionType) : Promise<string> {
-    const storage = getStorage(app);
-
-    try {
-        const csvRef = ref(storage, 'questions/'+questionType+'/'+questionType+'.csv');
-        getDownloadURL(csvRef).then((url) => {
-            fetch(url)
-                .then(response => response.text())
-                .then(data => {
-                    const lines = data.split('\n');
-                    let min = 1; 
-                    let max = 2;   
-                    let random = Math.floor(Math.random() * (max - min + 1)) + min;
-                    const firstLine = lines[random];
-                    console.log(firstLine);
-                    console.log('Data:', data);
-                    return Promise.resolve(firstLine);
-                })
-                .catch(error => {
-                    console.error('Error fetching CSV data:', error);
-                });
-        });
-    } catch (error) {
-        console.error('Error retrieving CSV file:', error);
-    }
-    return Promise.resolve('Error');
-};
-
 const QuestionContainer: React.FC<QuestionContainerProps> = ({ questionType }) => {
+    const [question, setQuestion] = useState<string>('');
+    const [choices, setChoices] = useState<string[]>([]); 
+    const [questionSet, setQuestionSet] = useState<boolean>(false);
+    
+    useEffect(() => {
+        if (!questionSet) {
+            console.log('Getting CSV file from storage');
+            getCSVFileFromStorage(questionType);
+            setQuestionSet(true);
+        }
+    })
 
-    // Call the function to get the CSV file from Firebase Storage
-    //const question = await getCSVFileFromStorage(questionType);
+    async function getCSVFileFromStorage(questionType) {
+        const storage = getStorage(app);
+
+        try {
+            const csvRef = ref(storage, 'questions/'+questionType+'/'+questionType+'.csv');
+            getDownloadURL(csvRef).then((url) => {
+                fetch(url)
+                    .then(response => response.text())
+                    .then(data => {
+                        const lines = data.split('\n');
+                        let min = 1; 
+                        let max = 2;   
+                        let random = Math.floor(Math.random() * (max - min + 1)) + min;
+                        const context = lines[random];
+                        console.log(context);
+    
+                        let info= context.split(',');
+                        setQuestion(info[0]);
+                        setChoices([info[1], info[2], info[3], info[4]]);
+                        setQuestionSet(true);
+                        //return Promise.resolve(firstLine);
+                    })
+                    .catch(error => {
+                        console.error('Error fetching CSV data:', error);
+                    });
+            });
+        } catch (error) {
+            console.error('Error retrieving CSV file:', error);
+        }
+    }
+
     if (questionType === 'mcq') {
 
-        return <MultipleChoiceQuestion question="What flavor of ice cream is your favorite?" choices={['Vanilla', 'Chocolate', 'Strawberry']} />;
-        //return <MultipleChoiceQuestion question={question} choices={choices} />;
+        //return <MultipleChoiceQuestion question="What flavor of ice cream is your favorite?" choices={['Vanilla', 'Chocolate', 'Strawberry']} />;
+        return <MultipleChoiceQuestion question={question} choices={choices} />;
 
     } else if (questionType === 'saq'){
-        return <TextQuestion question="What is your favorite color?" />;
+        return <TextQuestion question={question} />;
     } else {
         return <View>
                 <Text>Invalid Text.</Text>
