9aa157a2a2454a70264660fbd5dd950306e50fd6
Nicholas <nkpqiu@gmail.com>
2024-06-13 22:40:31 -0400

    remove api key

--- MVP/firebaseConfig.ts
+++ MVP/firebaseConfig.ts
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDSi2iZoYWNoDkjwYdkukUa0F-tR9xJWHk",
+  apiKey: "",
   authDomain: "mvp-5-4.firebaseapp.com",
   projectId: "mvp-5-4",
   storageBucket: "mvp-5-4.appspot.com",
