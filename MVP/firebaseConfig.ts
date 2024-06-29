f705a5267da7757f6c1d5a72fb67c6086a293866
KenZheng96 <124521720+KenZheng96@users.noreply.github.com>
2024-06-29 18:05:05 -0400

    setup

--- MVP/firebaseConfig.ts
+++ MVP/firebaseConfig.ts
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
+  apiKey: "AlzaSyDSi2iZoYWNoDkjwYd-kukUa0F-tR9xJWHK",
   authDomain: "mvp-5-4.firebaseapp.com",
   projectId: "mvp-5-4",
   storageBucket: "mvp-5-4.appspot.com",
